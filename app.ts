a85e32a30c7f0c216908ace01a859f1c63c4cd5b
NmnemoniX <yzhakovng@rambler.ru>
2022-03-03 07:30:27 +0500

    first

---
+++ app.ts
@@ -0,0 +1,27 @@
+
+
+interface IBook {
+    id: number;
+    title: string;
+    description: string;
+    authors: string;
+    favorite: string;
+    fileCover: string;
+    fileName: string;
+    fileBook: string;
+}
+
+class BookRepository {
+
+    createBook(book:IBook){
+
+    }
+
+}
+
+
+
+
+
+
+
