7929364dc404cee019d5a26feb96afc30df65e2b
NmnemoniX <yzhakovng@rambler.ru>
2022-03-03 20:11:09 +0500

    final 2 task

--- app.ts
+++ app.ts
@@ -13,8 +13,29 @@ interface IBook {
 
 class BookRepository {
 
-    createBook(book:IBook){
+    books:IBook[] ;
 
+    createBook(book:IBook){
+        this.books.push(book);
+    }
+    getBook(id:number){
+        return this.books.find(book=>book.id===id)
+    }
+    getBooks(){
+        return this.books
+    }
+    updateBook(book:IBook){
+        let updateitem = this.books.find(z=>z.id===book.id)
+        let index = this.books.indexOf(updateitem)
+
+        this.books[index]=book
+        return this.books[index]
+    }
+    deleteBook(id:number){
+        let deletitem = this.books.find(z=>z.id===id)
+        let index = this.books.indexOf(deletitem)
+        let deleItems = this.books.splice (index, 1);
+        return this.books
     }
 
 }
